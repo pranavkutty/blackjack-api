b4fd8e9d6ada668e9c3ab2286f85e8ec8da91229
Pranav Kutty <pranavkutty13@gmail.com>
2021-09-30 21:00:51 +0530

    initial setup

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+if (process.env.NODE_ENV !== 'production') require('dotenv').config()
+
+const mongoose = require('mongoose')
+
+//connect to db
+mongoose.connect(process.env.DATABASE_URL, { useUnifiedTopology: true, useNewUrlParser: true })
+db = mongoose.connection
+db.on('error', (error) => console.log(error))
+db.once('open', () => console.log('Connected to database'))
+
+app.use(express.json())
+
+app.listen(process.env.PORT || 8081, () => console.log("Server has started"));
